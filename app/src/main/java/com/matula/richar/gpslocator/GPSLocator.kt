1f7086bf5b83aff8dc6b797c03bb90966941787c
Richard <matula16@seznam.cz>
2018-01-08 21:21:58 +0100

    initial commit

---
+++ app/src/main/java/com/matula/richar/gpslocator/GPSLocator.kt
@@ -0,0 +1,12 @@
+package com.matula.richar.gpslocator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GPSLocator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gpslocator)
+    }
+}
